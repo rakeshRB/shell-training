7243444024b939aa3959ee92ec17e1d02c1872ec
B Mallikarjunappa <r.b.mallikarjunappa@accenture.com>
2016-02-20 23:23:25 +0530

    shell practice

---
+++ first.sh
@@ -0,0 +1,14 @@
+
+
+echo "This is my first script"
+
+echo enter x value:
+read x
+echo enter y value:
+read y
+
+total=`expr $x + $y`
+
+echo x value is: $x
+echo y value is: $y
+echo totla value: $total
---
+++ test.sh
@@ -0,0 +1,6 @@
+Sno Name Dep
+1 test1	DEV
+2 test2	Test
+3 test3	SIT
+4 test4	UAT
+5 test5	PROD
